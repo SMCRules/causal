3e0ba382e90f1e029f1c4bcde326c66340eb16ef
SMCRules <magbelmonte@googlemail.com>
2025-04-27 12:30:45 +0100

    adding headers to python file

--- guidance/caus_roncho.py
+++ guidance/caus_roncho.py
@@ -1,1 +1,21 @@
-""Hello tiu"
+import graphviz
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import shap
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import xgboost as xgb
+from functools import reduce
+from io import StringIO
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.models import BayesianNetwork
+from sklearn.metrics import roc_auc_score
+
+
+np.random.seed(100)
+
+# Only for the sake of cleaner output in this example
+logging.getLogger("pgmpy").setLevel(logging.ERROR)  
+
